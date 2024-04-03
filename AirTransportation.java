2975f3adea7f5a276ee561bc8ba1f686c2348cb9
Pedro0398 <pedro.178.lol@gmail.com>
2024-04-03 10:51:07 +0100

    Nivel 1

---
+++ AirTransportation.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class AirTransportation here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class AirTransportation
+{
+    // instance variables - replace the example below with your own
+    private String name;
+    private int numberOfContainers;
+    private final double time = 0.4;
+    public AirTransportation(String name, int numberOfContainers)
+    {
+        super();
+        this.name = name;
+        this.numberOfContainers = numberOfContainers;
+    }
+
+    
+}
---
+++ GroundTransportation.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class GroundTransportation here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class GroundTransportation
+{
+    private String licensePlate;
+    private final double time = 0.3;
+    
+    public GroundTransportation(String licensePlate)
+    {
+        super();
+        this.licensePlate = licensePlate;
+    }
+
+}
